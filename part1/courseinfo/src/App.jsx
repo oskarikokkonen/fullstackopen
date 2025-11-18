788cd4bee436d906e948dbd33cf8a27018df364d
Oskari Kokkonen <oskari.j.k@pm.me>
2025-11-18 20:13:55 +0200

    1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercise1={exercises1} 
+               part2={part2} exercise2={exercises2} 
+               part3={part3} exercise3={exercises3} />
+      <Total one={exercises1} two={exercises2} three={exercises3} />
+    </div>
+  )
+}
+
+const Header = (c) => {
+  return (
+    <p>
+      {c.course}
+    </p>
+  )
+}
+
+const Part = (c) => {
+  return (
+    <p>
+      {c.part} {c.exercise}
+    </p>
+  )
+}
+
+const Content = (c) => {
+  return (
+    <div>
+      <Part part={c.part1} exercise={c.exercise1} />
+      <Part part={c.part2} exercise={c.exercise2} />
+      <Part part={c.part3} exercise={c.exercise3} />
+    </div>
+  )
+}
+
+const Total = (c) => {
+  return (
+    <p>
+      Number of exercises {c.one + c.two + c.three}
+    </p>
   )
 }
 
