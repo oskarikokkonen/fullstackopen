725fc54267aea9c93e579d1233f88cb43c8d6bf7
Oskari Kokkonen <oskari.j.k@pm.me>
2026-02-20 13:39:36 +0200

    2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -14,7 +14,13 @@ const Part = ({part}) => (
   </p>
 )
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <strong>total of {total} exercises</strong>
+  )
+}
 
 
 const Course = ({course}) => {
@@ -22,6 +28,7 @@ const Course = ({course}) => {
     <div key={course.id}>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
